cc3987b886526ba2c80c6a84ffbac61f384d04fc
Omar Salih <oosalih98@gmail.com>
2026-02-20 13:24:46 -0500

    Edited FlightGenerationService

--- Services/FlightGenerationService.cs
+++ Services/FlightGenerationService.cs
@@ -1,10 +1,20 @@
-﻿namespace FlightBookingAPI.Services
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using FlightBookingAPI.Data;
+using FlightBookingAPI.Models;
+
+namespace FlightBookingAPI.Services
 {
     public class FlightGenerationService
     {
-        GenerateFlightsForNextDays(int days)
+        public List<FlightInstance> GenerateFlightsForNextDays(int days, List<FlightInstance>? existingFlights = null)
         {
+            var templates = SeedData.GetTemplates();
+            var routes = SeedData.GetRoutes();
 
+            var existingNumber = new HashSet<string>(existingFlights .Where(f = >);
         }
     }
 }
